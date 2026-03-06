9bf9bd498f63b560644e418be02a868165d30550
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-03-06 21:57:10 +0600

    Implement nav active color

--- script/script.js
+++ script/script.js
@@ -1,1 +1,16 @@
 
+
+const buttonContainer = document.getElementById('buttonContainer')
+
+buttonContainer.addEventListener('click', (e) => {
+
+    if (e.target.localName === 'button') {
+        const buttons = document.querySelectorAll('.btn-nav')
+        buttons.forEach(btn => {
+            btn.classList.remove('btn-primary')
+        })
+        e.target.classList.add('btn-primary')
+    }
+
+
+})
